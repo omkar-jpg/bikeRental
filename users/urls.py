5c6a8de6579321b5991efd802ae79e9299a361d2
ayushneupane312 <ayushneupane2022@gmail.com>
2025-05-14 17:44:47 +0545

    user profile page and info completed successfully ,         bike description of various bikes added to databse,             leaflet map added to home page,

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from django.conf import settings
-from . import views
-from django.conf.urls.static import static
+from django.urls import path  # Import the path function to define URL patterns
+from django.conf import settings  # Import settings to access MEDIA_URL and MEDIA_ROOT
+from . import views  # Import views from the current app
+from django.conf.urls.static import static  # Import static to serve media files in development
 
 urlpatterns = [
-    path('profile/', views.profile_view, name ='profile'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('profile/', views.profile_view, name ='profile'),  # URL pattern for the profile view
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serve media files during development
